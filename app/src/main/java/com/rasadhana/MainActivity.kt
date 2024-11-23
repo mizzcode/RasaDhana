3c222ee927d32b863bde1dcb4c0804734d1cf1e6
Misbahudin <misbahudin.id@gmail.com>
2024-11-23 17:24:45 +0700

    feat: add ui foto bahan

--- app/src/main/java/com/rasadhana/MainActivity.kt
+++ app/src/main/java/com/rasadhana/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.rasadhana
 
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -19,20 +20,26 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        supportActionBar?.hide()
-
         val navView: BottomNavigationView = binding.navView
-        navView.itemBackground = null
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_favorite,  R.id.navigation_camera, R.id.navigation_kulkas, R.id.navigation_account
+                R.id.navigation_home, R.id.navigation_favorite,  R.id.navigation_photo, R.id.navigation_kulkas, R.id.navigation_account
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    fun setBottomNavVisibility(visible: Boolean) {
+        binding.navCard.visibility = if (visible) View.VISIBLE else View.GONE
     }
 }
---
+++ app/src/main/java/com/rasadhana/ui/photo/PhotoFragment.kt
@@ -0,0 +1,138 @@
+package com.rasadhana.ui.photo
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.addCallback
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
+import com.rasadhana.MainActivity
+import com.rasadhana.R
+import com.rasadhana.databinding.FragmentPhotoBinding
+import com.rasadhana.getImageUri
+import org.koin.android.ext.android.inject
+
+class PhotoFragment : Fragment() {
+    private var _binding: FragmentPhotoBinding? = null
+    private val binding get() = _binding!!
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                showToast("Permission request granted")
+            } else {
+                showToast("Permission request denied")
+            }
+        }
+
+    private val viewModel: PhotoViewModel by inject()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentPhotoBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        (requireActivity() as MainActivity).setBottomNavVisibility(false)
+
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (!allPermissionsGranted()) {
+            requestPermissionLauncher.launch(REQUIRED_PERMISSION)
+        }
+
+        (activity as AppCompatActivity).supportActionBar?.apply {
+            title = getString(R.string.photo_ingredient)
+            setDisplayHomeAsUpEnabled(true)
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            findNavController().navigateUp()
+        }
+
+        binding.galleryButton.setOnClickListener { startGallery() }
+        binding.cameraButton.setOnClickListener { startCamera() }
+//        binding.uploadButton.setOnClickListener { uploadImage() }
+    }
+
+
+    private fun allPermissionsGranted() =
+        ContextCompat.checkSelfPermission(
+            requireContext(),
+            REQUIRED_PERMISSION
+        ) == PackageManager.PERMISSION_GRANTED
+
+    private fun startGallery() {
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            viewModel.currentImageUri = uri
+            showImage()
+        } else {
+            Log.d("Photo Picker", "No media selected")
+        }
+    }
+
+    private fun showImage() {
+        viewModel.currentImageUri?.let {
+            Log.d("Image URI", "showImage: $it")
+            binding.previewImageView.setImageURI(it)
+        }
+    }
+
+    private fun startCamera() {
+        Log.d("Photo Fragment", viewModel.currentImageUri.toString())
+        viewModel.currentImageUri = getImageUri(requireContext())
+        launcherIntentCamera.launch(viewModel.currentImageUri!!)
+    }
+
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.TakePicture()
+    ) { isSuccess ->
+        if (isSuccess) {
+            showImage()
+        } else {
+            viewModel.currentImageUri = null
+        }
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        binding.progressIndicator.visibility = if (isLoading) View.VISIBLE else View.GONE
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+    }
+
+    companion object {
+        private const val REQUIRED_PERMISSION = Manifest.permission.CAMERA
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+        (requireActivity() as MainActivity).setBottomNavVisibility(true)
+    }
+}
