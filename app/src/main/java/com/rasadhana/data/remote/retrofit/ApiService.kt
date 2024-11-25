2609ef2fb26398b7c17e40268139fed690f02790
Misbahudin <misbahudin.id@gmail.com>
2024-11-25 19:22:26 +0700

    feat: add register and login

--- app/src/main/java/com/rasadhana/data/remote/retrofit/ApiService.kt
+++ app/src/main/java/com/rasadhana/data/remote/retrofit/ApiService.kt
@@ -1,5 +1,24 @@
 package com.rasadhana.data.remote.retrofit
 
-interface ApiService {
+import com.rasadhana.data.remote.response.LoginResponse
+import com.rasadhana.data.remote.response.RegisterResponse
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
 
+interface ApiService {
+    @FormUrlEncoded
+    @POST("auth/register")
+    suspend fun register(
+        @Field("name") name: String,
+        @Field("email") email: String,
+        @Field("password") password: String
+    ) : RegisterResponse
+
+    @FormUrlEncoded
+    @POST("auth/login-user")
+    suspend fun login(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ) : LoginResponse
 }
--- app/src/main/java/com/rasadhana/data/repository/UserRepository.kt
+++ app/src/main/java/com/rasadhana/data/repository/UserRepository.kt
@@ -1,7 +1,51 @@
 package com.rasadhana.data.repository
 
+import androidx.lifecycle.liveData
+import com.google.gson.Gson
+import com.rasadhana.data.Result
+import com.rasadhana.data.pref.UserModel
+import com.rasadhana.data.pref.UserPreference
+import com.rasadhana.data.remote.response.RegisterResponse
 import com.rasadhana.data.remote.retrofit.ApiService
+import kotlinx.coroutines.flow.Flow
+import retrofit2.HttpException
 
-class UserRepository(private val apiService: ApiService) {
+class UserRepository(private val apiService: ApiService, private val userPreference: UserPreference) {
+    fun register(name: String, email: String, password: String) = liveData {
+        emit(Result.Loading)
 
+        try {
+            val response = apiService.register(name, email, password)
+            emit(Result.Success(response))
+        } catch (e: HttpException) {
+            val errorBody = e.response()?.errorBody()?.string()
+            val errorResponse = Gson().fromJson(errorBody, RegisterResponse::class.java)
+            emit(Result.Error(errorResponse.message))
+        }
+    }
+
+    fun login(email: String, password: String) = liveData {
+        emit(Result.Loading)
+
+        try {
+            val response = apiService.login(email, password)
+            emit(Result.Success(response))
+        } catch (e: HttpException) {
+            val errorBody = e.response()?.errorBody()?.string()
+            val errorResponse = Gson().fromJson(errorBody, RegisterResponse::class.java)
+            emit(Result.Error(errorResponse.message))
+        }
+    }
+
+    suspend fun saveSession(user: UserModel) {
+        userPreference.saveSession(user)
+    }
+
+    fun getSession(): Flow<UserModel> {
+        return userPreference.getSession()
+    }
+
+    suspend fun logout() {
+        userPreference.logout()
+    }
 }
