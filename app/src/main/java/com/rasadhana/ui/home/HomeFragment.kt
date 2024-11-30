249a8ecee5288739d0c277f2767bc9ce3816971c
Misbahudin <misbahudin.id@gmail.com>
2024-11-30 13:46:05 +0700

    fix: ux back button

--- app/src/main/java/com/rasadhana/ui/home/HomeFragment.kt
+++ app/src/main/java/com/rasadhana/ui/home/HomeFragment.kt
@@ -1,13 +1,19 @@
 package com.rasadhana.ui.home
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import android.widget.Toast
+import androidx.activity.addCallback
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.search.SearchView.TransitionState
 import com.rasadhana.R
 import com.rasadhana.data.Result
 import com.rasadhana.databinding.FragmentHomeBinding
@@ -45,6 +51,46 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            searchView.setupWithSearchBar(searchBar)
+
+            searchView.editText.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+                override fun afterTextChanged(s: Editable?) {
+                    val query = s.toString()
+                    showToast(query)
+                }
+            })
+
+            searchView.editText.setOnEditorActionListener { _, actionId, _ ->
+                if (actionId == EditorInfo.IME_ACTION_SEARCH || actionId == EditorInfo.IME_ACTION_DONE) {
+                    showToast(searchView.text.toString())
+                    searchView.hide()
+                    (requireActivity() as MainActivity).setBottomNavVisibility(true)
+                }
+                false
+            }
+
+            searchView.addTransitionListener { _, _, newState ->
+                if (newState === TransitionState.SHOWING) {
+                    (requireActivity() as MainActivity).setBottomNavVisibility(false)
+                } else if (newState === TransitionState.HIDING) {
+                    (requireActivity() as MainActivity).setBottomNavVisibility(true)
+                }
+            }
+
+            requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+                if (searchView.isVisible) {
+                    searchView.hide()
+                    (requireActivity() as MainActivity).setBottomNavVisibility(true)
+                } else {
+                    isEnabled = false
+                    requireActivity().onBackPressedDispatcher.onBackPressed()
+                }
+            }
+        }
 
         val recipeSuggestionAdapter = RecipeSuggestionAdapter()
 
