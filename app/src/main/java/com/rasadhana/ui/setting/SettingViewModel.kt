738e6b3a8493a3d36a2b646834a9c54007de30b8
Misbahudin <misbahudin.id@gmail.com>
2024-12-11 13:14:47 +0700

    feat: add update user

--- app/src/main/java/com/rasadhana/ui/setting/SettingViewModel.kt
+++ app/src/main/java/com/rasadhana/ui/setting/SettingViewModel.kt
@@ -2,11 +2,20 @@ package com.rasadhana.ui.setting
 
 import android.net.Uri
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.rasadhana.data.pref.UserModel
 import com.rasadhana.data.repository.UserRepository
+import kotlinx.coroutines.launch
 import java.io.File
 
 class SettingViewModel(private val userRepository: UserRepository) : ViewModel() {
     var currentImageUri: Uri? = null
 
-    fun updateUser(photo: File, name: String, userId: String) = userRepository.updateUser(photo, name, userId)
+    fun saveSession(user: UserModel) {
+        viewModelScope.launch {
+            userRepository.saveSession(user)
+        }
+    }
+
+    fun updateUser(photo: File?, name: String?, userId: String) = userRepository.updateUser(photo, name, userId)
 }
